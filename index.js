17f9b9421e4069102e7114aa314dd362f1f7f6bc
anojroshan <73768440+anojroshan@users.noreply.github.com>
2021-10-08 08:43:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-Parser')
+const app = express()
+// Middleware 
+app.use(bodyParser.urlencoded({ extended: false }));
+const PORT = process.env.PORT || 3000;
+app.get('/login',(req,res)=>{
+    res.sendFile(__dirname+'/index.html')
+})
+app.post('/login',(req,res)=>{
+    const username = req.body.username
+    res.send("hello  " + username)
+})
+app.listen(3000,()=>{
+    console.log("Working fine!");
+})
